a9ddf78753bb91ed879a7c17d74e44cbb8a98a3a
Ssaj525 <141895840+Ssaj525@users.noreply.github.com>
2023-08-16 20:23:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,5 @@ var datetime = "Mark Attendance As On: " + currentdate.getDay() + "/" + currentd
 
 document.querySelector(".date").innerHTML = datetime;
 
-document.getElementById("download-btn").addEventListener("click",function(){
-    var element = document.getElementById('element-to-print');
-var opt = {
-  margin:       1,
-  filename:     'myfile.pdf',
-  image:        { type: 'jpeg', quality: 0.98 },
-  html2canvas:  { scale: 2 },
-  jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
-};
-html2pdf().set(opt).from(element).save();
-});
-
 
 // New Promise-based usage:
