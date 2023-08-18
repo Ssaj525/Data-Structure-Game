6b863af701d788385e7195f91cb4a808fe5372e5
Ssaj525 <141895840+Ssaj525@users.noreply.github.com>
2023-08-18 11:00:09 +0530

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-var currentdate = new Date();
-var datetime = "Mark Attendance As On: " + currentdate.getDay() + "/" + currentdate.getMonth() 
-+ "/" + currentdate.getFullYear() + " at" + " " 
-+ currentdate.getHours() % 12 + ":" 
-+ currentdate.getMinutes() + ":" + currentdate.getSeconds();
-
+var currentdate = new Date(); 
+var datetime = "Mark Attendance as on : " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " at "  
+                + currentdate.getHours() % 12 + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
 document.querySelector(".date").innerHTML = datetime;
+
 
 
 // New Promise-based usage:
